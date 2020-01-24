225c1077b89f3021b48ac69eb5713e6e057e2df8
Hanna Lee <mani21na@gmail.com>
2020-01-24 05:57:24 +0000

    Automatically backed up by Learn

--- app/models/student.rb
+++ app/models/student.rb
@@ -2,14 +2,6 @@ class Student < ActiveRecord::Base
   def to_s
     self.first_name + " " + self.last_name
   end
-  
-  def check_active
-    if self.active
-      "This student is currently active."
-    else
-      "This student is currently inactive."
-    end
-  end
 
   def change_active
     if self.active
